b5737e51640a85ee3c5f2c1cbbff49e49fc6e60b
seven <diangelisvida@gmail.com>
2024-06-04 15:24:32 -0300

    primeiro commit nivel-heroi

---
+++ script.js
@@ -0,0 +1,27 @@
+let heroi = "Strange";
+let exp = 9000;
+
+if(exp<=1000){
+  console.log("O Herói de nome " +heroi+ " está no nível Ferro")
+
+}else if(exp>=1001 && exp<=2000){
+  console.log("O Herói de nome " +heroi+ " está no nível Bronze")
+
+}else if(exp>=2001 && exp<=5000){
+  console.log("O Herói de nome " +heroi+ " está no nível Prata")
+
+}else if(exp>=5001 && exp<=7000){
+  console.log("O Herói de nome " +heroi+ " está no nível Ouro")
+
+}else if(exp>=7001 && exp<=8000){
+  console.log("O Herói de nome " +heroi+ " está no nível Platina")
+
+}else if(exp>=8001 && exp<=9000){
+  console.log("O Herói de nome " +heroi+ " está no nível Ascendente")
+
+}else if(exp>=9001 && exp<=10000){
+  console.log("O Herói de nome " +heroi+ " está no nível Imortal")
+
+}else if(exp>=10001){
+  console.log("O Herói de nome " +heroi+ " está no nível Radiante")
+}
